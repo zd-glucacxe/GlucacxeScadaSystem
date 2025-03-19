aa4359d4e3a90608ba23f9a0d8ba4fe9c119ed8b
Glucacxe <Glucacxe@DESKTOP-QBACPO2>
2025-03-19 23:55:33 +0800

    添加项目文件。

---
+++ GlucacxeScadaSystem/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using GlucacxeScadaSystem.ViewModels;
+using GlucacxeScadaSystem.Views;
+using Prism.Ioc;
+
+namespace GlucacxeScadaSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            //return Container.Resolve<MainWindow>();
+            return Container.Resolve<ShellView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // 注册单例
+            containerRegistry.RegisterSingleton<ShellViewModel>();
+        }
+    }
+}
